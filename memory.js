5ee8ed29344413ca291cabc8b5e67fd75f40e763
abodziacka <58439913+abodziacka@users.noreply.github.com>
2020-03-20 16:05:22 +0100

    Add files via upload

---
+++ memory.js
@@ -0,0 +1,91 @@
+var cards = document.querySelectorAll('.memory-card');
+
+var hasFlippedCard = false;
+var lockBoard = false;
+var firstCard, secondCard;
+var moveCount = 0;
+var wins = 0;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+
+    checkForMatch();
+
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        this.wins++;
+        disableCards();
+        this.moveCount++;
+        document.getElementById("game-score").innerHTML = this.moveCount;
+
+        DoYouWin();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1000);
+
+    this.moveCount++;
+    document.getElementById("game-score").innerHTML = this.moveCount;
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        var randomPos = Math.floor(Math.random() * 16);
+        card.style.order = randomPos;
+    });
+})();
+
+function DoYouWin() {
+    if (this.wins === 8) {
+        document.getElementById("button").style.display = "block";
+        document.getElementById("brawo").style.display="block";
+    
+    }
+    return;
+}
+
+function Reset(){
+    this.cards = document.querySelectorAll('.memory-card');
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+    this.moveCount = 0;
+    this.wins = 0;
+
+    flipCard();
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
+
